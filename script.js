27c93bebd1538e4fb6f886e2d36595fa3d0480ca
Richard <richardzehungho@gmail.com>
2022-09-17 22:48:00 -0700

    Add mouse event listeners to simulate drawing

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const drawContainer = document.querySelector('main');
 const slider = document.querySelector('.slider');
 const output = document.querySelector('.output');
 let update = () => output.textContent = parseInt(slider.value); 
-
-// const colour = document.querySelector('.column');
+const squares = document.querySelectorAll('.column');
+let isDrawing = false;
 
 
 slider.addEventListener('input', changeSlide);
@@ -11,12 +11,14 @@ slider.addEventListener('input', changeGrid);
 slider.addEventListener('input', update);
 
 function changeGrid () {
+    // Creates rows of empty divs
     for (let i = 0; i < parseInt(slider.value); i++) {
         const rows = document.createElement('div');
         rows.classList.add('rows');
         rows.style.width = '100%';
         rows.style.display = "flex";
         drawContainer.append(rows);
+        // fills the div rows with empty div boxes
         for (let j = 0; j < parseInt(slider.value); j++) {
             const column = document.createElement('div');
             column.classList.add('column');
@@ -24,13 +26,26 @@ function changeGrid () {
             column.style.border = "solid black";
             rows.appendChild(column);
             
-            // Changes colour
-            column.addEventListener('mousedown', changeColour);
+            // Changes colour 
+            column.addEventListener('mousedown',changeColour);
             function changeColour(){
-
-                column.style.backgroundColor = "blue";
+                column.style.backgroundColor = "black";
             }
-
+            column.addEventListener('mousedown', (e) =>{
+                changeColour;
+                isDrawing = true;
+            });
+            column.addEventListener('mousemove', (e) => {
+                if(isDrawing) {
+                    column.addEventListener('mousemove', changeColour);
+                }
+            });
+            column.addEventListener('mouseup', (e) => {
+                if(isDrawing){
+                    changeColour;
+                    isDrawing = false;
+                }
+            });
         }
     }
 }
@@ -54,4 +69,7 @@ create loop to add certain amount of div containers to playarea
 create slider to adjust amount of grid,
 use that input into for loop,
 clear grid each time slider has new value,
+
+add an event listener to add colour to the column divs
+create way so mousedown can constantly fill divs
 */
