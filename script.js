b072bae98009ac40d013d0f03cf54af60079d110
Richard <richardzehungho@gmail.com>
2022-09-19 22:58:03 -0700

    Add function to reset grid and add html buttons

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const drawContainer = document.querySelector('main');
+let update = () => output.textContent = parseInt(slider.value); 
+let isDrawing = false;
+let buttonClick = false;
+
+
+//slider variables and events
 const slider = document.querySelector('.slider');
 const output = document.querySelector('.output');
-let update = () => output.textContent = parseInt(slider.value); 
-const squares = document.querySelectorAll('.column');
-let isDrawing = false;
-
-
 slider.addEventListener('input', changeSlide);
 slider.addEventListener('input', changeGrid);
 slider.addEventListener('input', update);
+
+//button variables and events
+const colourPicker = document.querySelector('.colourPicker');
+const colourBtn = document.querySelector('.colourPicker');
+const rainbowPicker = document.querySelector('.rainbowPicker');
+const reset = document.querySelector('.reset');
+
+rainbowPicker.addEventListener('click', () =>{
+    buttonClick = !buttonClick;
+    console.log(buttonClick);
+});
 
 function changeGrid () {
     // Creates rows of empty divs
@@ -26,11 +38,18 @@ function changeGrid () {
             column.style.border = "solid black";
             rows.appendChild(column);
             
-            // Changes colour 
+            // Creates initial click and sets colour of square
             column.addEventListener('mousedown',changeColour);
+            // column.addEventListener('mousedown',rainbowColour);
             function changeColour(){
-                column.style.backgroundColor = "black";
-            }
+                column.style.backgroundColor = colourPicker.value;
+            };
+            function rainbowColour(){
+                let randomColour = Math.floor(Math.random()*16777215).toString(16);
+                column.style.backgroundColor = '#' + randomColour;
+            };
+
+            //creates the drawing effect by using mouse events to finding if variable is true or not
             column.addEventListener('mousedown', (e) =>{
                 changeColour;
                 isDrawing = true;
@@ -46,15 +65,48 @@ function changeGrid () {
                     isDrawing = false;
                 }
             });
+
+            rainbowPicker.addEventListener('click', (e) =>{
+                column.addEventListener('mousedown', (e) =>{
+                    rainbowColour;
+                    isDrawing = true;
+                });
+                column.addEventListener('mousemove', (e) => {
+                    if(isDrawing) {
+                        column.addEventListener('mousemove', rainbowColour);
+                    }
+                });
+                column.addEventListener('mouseup', (e) => {
+                    if(isDrawing){
+                        rainbowColour;
+                        isDrawing = false;
+                    }
+                });
+            }) 
+
+            reset.addEventListener('click', resetGrid);
+            function resetGrid(){
+                column.style.backgroundColor = null;
+                column.removeEventListener('mousemove',changeColour);
+                column.removeEventListener('mousemove',rainbowColour);
+            }
+            
+
         }
     }
 }
+
 
 function changeSlide() {
     while(drawContainer.hasChildNodes()){
         drawContainer.removeChild(drawContainer.lastChild);
     }
 }
+
+
+
+
+
 
 
 
